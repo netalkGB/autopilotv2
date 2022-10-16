17008c8b95dce14440b4f39944141e8f38344d65
gb <13611993+netalkGB@users.noreply.github.com>
2022-10-16 23:32:50 +0900

    webpack5非対応なのでwebpack-filter-warnings-pluginを使用しない

--- webpilot/webpack.config.js
+++ webpilot/webpack.config.js
@@ -1,7 +1,6 @@
 const path = require("path");
 const webpack = require("webpack");
 const ESlintWebpackPlugin = require("eslint-webpack-plugin");
-const FilterWarningsPlugin = require('webpack-filter-warnings-plugin');
 
 module.exports = (_, argv) => {
   const mode = argv.mode === 'development' ? 'development' : 'production'
@@ -38,16 +37,29 @@ module.exports = (_, argv) => {
           filename: "index.js"
         },
         plugins: [
-          // TypeORMのfaqにあるとおり握潰
-          new FilterWarningsPlugin({
-            exclude: [/mongodb/, /mssql/, /mysql/, /mysql2/, /oracledb/, /pg/, /pg-native/, /pg-query-stream/, /react-native-sqlite-storage/, /redis/, /sqlite3/, /sql.js/, /typeorm-aurora-data-api-driver/, /@sap\/hana-client/, /hdb-pool/, /@google-cloud\/spanner/]
-          }),
-          // Module not found: Error: Can't resolve 'pg-native'なので握潰（なくても動く）
-          new webpack.IgnorePlugin({ resourceRegExp: /^pg-native$/}),
+          // TypeORMのfaqにあるとおり握潰（FilterWarningsPluginがWebpack5で非対応のため、IgnorePluginで代用）
+          ...([
+            /^mongodb$/,
+            /^mssql$/,
+            /^mysql$/,
+            /^mysql2$/,
+            /^oracledb$/,
+            // /^pg$/,
+            /^pg-native$/,
+            /^pg-query-stream$/,
+            /^react-native-sqlite-storage$/,
+            /^redis$/,
+            /^sqlite3$/,
+            /^sql.js$/,
+            /^typeorm-aurora-data-api-driver$/,
+            /^@sap\/hana-client$/,
+            /^hdb-pool$/,
+            /^@google-cloud\/spanner$/
+          ].map(ign => (new webpack.IgnorePlugin({ resourceRegExp: ign })))),
           new ESlintWebpackPlugin({
             extensions: ["js", "ts"],
             fix: true
           })
-        ]
+        ],
     };
 }
