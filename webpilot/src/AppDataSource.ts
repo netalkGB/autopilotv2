09201a1ed6a167e34327864dd9dd26aba60d73b2
gb <13611993+netalkGB@users.noreply.github.com>
2022-11-04 01:05:54 +0900

    AppDataSourceへの追加を忘れていたため指定した

--- webpilot/src/AppDataSource.ts
+++ webpilot/src/AppDataSource.ts
@@ -5,6 +5,14 @@ import { Pilot } from './entity/autopilot/Pilot'
 import { Schedule } from './entity/autopilot/Schedule'
 import { Config } from './entity/autopilot/Config'
 import dbConfig from '../dbConfig.json'
+import { Client } from './entity/auth/Client'
+import { TmpAccessToken } from './entity/auth/TmpAccessToken'
+import { TmpAuthToken } from './entity/auth/TmpAuthToken'
+import { TmpCode } from './entity/auth/TmpCode'
+import { TmpLoginToken } from './entity/auth/TmpLoginToken'
+import { TmpRefreshToken } from './entity/auth/TmpRefreshToken'
+import { User } from './entity/auth/User'
+import { UserInfo } from './entity/auth/UserInfo'
 
 const {
   host,
@@ -23,5 +31,18 @@ export const AppDataSource = new DataSource({
   database,
   synchronize: true,
   logging: true,
-  entities: [Schedule, Pilot, History, Config]
+  entities: [
+    Schedule,
+    Pilot,
+    History,
+    Config,
+    Client,
+    TmpAccessToken,
+    TmpAuthToken,
+    TmpCode,
+    TmpLoginToken,
+    TmpRefreshToken,
+    User,
+    UserInfo
+  ]
 })
