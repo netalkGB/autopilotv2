e132d8f3816b789f0c6af7fcb60906c3c93a2d07
gb <13611993+netalkGB@users.noreply.github.com>
2022-11-04 02:36:09 +0900

    ランダム値生成関数を追加

--- webpilot/src/utils/AppUtils.ts
+++ webpilot/src/utils/AppUtils.ts
@@ -1,7 +1,16 @@
-const { randomUUID } = require('crypto')
+import randomstring from 'randomstring'
+import { randomUUID } from 'crypto'
 
 export class AppUtils {
   public static generateUUID (): string {
     return randomUUID()
+  }
+
+  public static generateLoginToken (): string {
+    return this.generateRandomString(8)
+  }
+
+  public static generateRandomString (length: number): string {
+    return randomstring.generate(length)
   }
 }
