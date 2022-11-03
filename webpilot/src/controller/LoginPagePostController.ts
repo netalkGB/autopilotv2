98a58560ba6c9fb5cc8345a27a9f0408019629fd
gb <13611993+netalkGB@users.noreply.github.com>
2022-11-04 02:37:08 +0900

    とりあえず、ID打って存在したらログインできたことにする

--- webpilot/src/controller/LoginPagePostController.ts
+++ webpilot/src/controller/LoginPagePostController.ts
@@ -1,5 +1,9 @@
 import { Request, Response } from 'express'
 import log4js from 'log4js'
+import { AppDataSource } from '../AppDataSource'
+import { User } from '../entity/auth/User'
+import { AppUtils } from '../utils/AppUtils'
+import { MailServiceImpl } from '../service/MailServiceImpl'
 
 const logger = log4js.getLogger('app')
 
@@ -10,6 +14,31 @@ export const loginPagePostController = async (request: Request, response: Respon
     response.send('A problem was encountered in login!').status(400).end()
     return
   }
-  response.render('login', { data: { error: true } })
+  // CSRFトークン生成
+  const csrfToken = AppUtils.generateUUID()
+  // CSRFトークンをセッションに保存
+  request.session.csrfToken = csrfToken
+
+  const id = request.body.id
+  logger.debug(`id: ${id}`)
+  if (!id) {
+    response.render('login', { data: { csrfToken, error: true, message: 'Please enter your id.' } })
+    return
+  }
+  // TODO: あとでサービスにする
+  const userRepository = AppDataSource.getRepository(User)
+  const user = await userRepository.createQueryBuilder().select().where('id = :id', { id }).getOne()
+  logger.debug(`user: ${JSON.stringify(user)}`)
+  if (user === null) {
+    logger.info(`That id is not registered: ${id}`)
+    response.render('login', { data: { csrfToken, error: true, message: 'That id is not registered.' } })
+    return
+  }
+  const mailService = new MailServiceImpl(logger)
+  await mailService.send(user.email, 'autopilot authentication', `login code is ${AppUtils.generateLoginToken()}`)
+  // ユーザーIDをセッションに格納
+  request.session.username = user.id
+
+  response.redirect('/')
   logger.info('end loginPagePostController')
 }
