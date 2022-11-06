f5bf49e18dbea51b2d0ee72fc72aeba8299e0248
gb <13611993+netalkGB@users.noreply.github.com>
2022-11-06 20:06:12 +0900

    userNameをuserIdに修正

--- webpilot/src/controller/IndexPageGetController.ts
+++ webpilot/src/controller/IndexPageGetController.ts
@@ -5,13 +5,13 @@ const logger = log4js.getLogger('app')
 
 export const indexPageGetController = async (request: Request, response: Response) => {
   logger.info('start indexPageGetController')
-  if (!request.session?.username) {
+  if (!request.session?.userId) {
     logger.debug('redirect to /login')
     response.redirect('/login')
     logger.info('end indexPageGetController')
     return
   }
 
-  response.render('index', { data: { username: request.session.username } })
+  response.render('index', { data: { username: request.session.userId } })
   logger.info('end indexPageGetController')
 }
