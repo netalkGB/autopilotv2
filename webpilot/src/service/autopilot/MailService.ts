975e2d629fac367a6ea6f79172c3a768f6296436
gb <13611993+netalkGB@users.noreply.github.com>
2022-11-08 23:31:22 +0900

    引数間違ってた

--- webpilot/src/service/autopilot/MailService.ts
+++ webpilot/src/service/autopilot/MailService.ts
@@ -1,3 +1,3 @@
 export interface MailService {
-  send(from: string, to: string, subject: string, text: string): Promise<boolean>
+  send(to: string, subject: string, text: string): Promise<boolean>
 }
