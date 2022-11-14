7d6d7f2bfcda6217bdbba0c83d6efd77ef093800
gb <13611993+netalkGB@users.noreply.github.com>
2022-11-15 02:27:38 +0900

    ベアラ-トークン生成関数を追加

--- webpilot/src/utils/AppUtils.ts
+++ webpilot/src/utils/AppUtils.ts
@@ -14,6 +14,10 @@ export class AppUtils {
     return this.generateRandomString(8)
   }
 
+  public static generateBearerToken (): string {
+    return this.generateRandomString(32)
+  }
+
   public static generateS256CodeChallenge (codeVerifier: string): string {
     const b64ToB64Url = (b64: string) => b64.replace(/\//g, '_').replace(/\+/g, '-').replace(/=/g, '')
     return b64ToB64Url(crypto.createHash('sha256').update(codeVerifier).digest('base64'))
