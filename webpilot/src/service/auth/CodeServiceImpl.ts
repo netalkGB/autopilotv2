403866ccd5f579d3ac9bd3d733be66bb73acf86b
gb <13611993+netalkGB@users.noreply.github.com>
2022-11-16 22:59:49 +0900

    repositoryを使うよう修正

--- webpilot/src/service/auth/CodeServiceImpl.ts
+++ webpilot/src/service/auth/CodeServiceImpl.ts
@@ -1,17 +1,15 @@
 import { CodeService } from './CodeService'
-import { DataSource, EntityManager, Repository } from 'typeorm'
+import { DataSource, Repository } from 'typeorm'
 import { TmpCode } from '../../entity/auth/TmpCode'
 
 export class CodeServiceImpl implements CodeService {
   private tmpCodeRepository: Repository<TmpCode>
-  private entityManager: EntityManager
   constructor (appDataSource: DataSource) {
     this.tmpCodeRepository = appDataSource.getRepository(TmpCode)
-    this.entityManager = appDataSource.manager
   }
 
   public async fetchCode (code: string): Promise<TmpCode | null> {
-    return await this.entityManager.createQueryBuilder().select().where('code = :code', { code }).getOne()
+    return await this.tmpCodeRepository.createQueryBuilder().select().where('code = :code', { code }).getOne()
   }
 
   public async insertCode (code: string, clientId: string, userId: string, scope: string, codeChallengeMethod: string, codeChallenge: string, nonce: string, created: Date): Promise<void> {
@@ -28,6 +26,6 @@ export class CodeServiceImpl implements CodeService {
   }
 
   public async deleteCode (code: string): Promise<void> {
-    await this.entityManager.createQueryBuilder().delete().from(TmpCode).where('code = :code', { code }).execute()
+    await this.tmpCodeRepository.createQueryBuilder().delete().where('code = :code', { code }).execute()
   }
 }
