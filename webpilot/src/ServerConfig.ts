6f4abd4b265c763d09979733fe8a621685cc0692
gb <13611993+netalkGB@users.noreply.github.com>
2022-11-17 00:34:22 +0900

    設定ファイルのクラスを修正

--- webpilot/src/ServerConfig.ts
+++ webpilot/src/ServerConfig.ts
@@ -1,5 +1,9 @@
 export class ServerConfig {
   static port: number
   static proxyUrl: string
+  static serverAddress: string
   static mailConfig: any
+  static serverRsaKey: any
+  static accessTokenExpireInS: number
+  static idTokenExpireInS: number
 }
