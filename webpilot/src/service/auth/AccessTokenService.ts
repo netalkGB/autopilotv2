3ff58396dacdce7bc4187c2a7a93dc93a215b34d
gb <13611993+netalkGB@users.noreply.github.com>
2022-11-19 00:40:16 +0900

    アクセストークンからアクセストークンを取得するメソッドを追加

--- webpilot/src/service/auth/AccessTokenService.ts
+++ webpilot/src/service/auth/AccessTokenService.ts
@@ -1,3 +1,6 @@
+import { TmpAccessToken } from '../../entity/auth/TmpAccessToken'
+
 export interface AccessTokenService {
   insertAccessToken(accessToken: string, userId: string, clientId: string, scope: string, refreshToken: string, expiresInS: number): Promise<void>
+  fetchAccessTokenByToken(accessToken: string): Promise<TmpAccessToken | null>
 }
--- webpilot/src/service/auth/AccessTokenServiceImpl.ts
+++ webpilot/src/service/auth/AccessTokenServiceImpl.ts
@@ -1,11 +1,17 @@
-import { DataSource, EntityManager } from 'typeorm'
+import { DataSource, EntityManager, Repository } from 'typeorm'
 import { TmpAccessToken } from '../../entity/auth/TmpAccessToken'
 import { AccessTokenService } from './AccessTokenService'
 
 export class AccessTokenServiceImpl implements AccessTokenService {
   private entityManager: EntityManager
+  private tmpAccessTokenRepository: Repository<TmpAccessToken>
   constructor (appDataSource: DataSource) {
     this.entityManager = appDataSource.manager
+    this.tmpAccessTokenRepository = appDataSource.getRepository(TmpAccessToken)
+  }
+
+  public async fetchAccessTokenByToken (accessToken: string): Promise<TmpAccessToken | null> {
+    return await this.tmpAccessTokenRepository.createQueryBuilder().select().where('access_token = :accessToken', { accessToken }).getOne()
   }
 
   public async insertAccessToken (accessToken: string, userId: string, clientId: string, scope: string, refreshToken: string, expiresInS: number): Promise<void> {
