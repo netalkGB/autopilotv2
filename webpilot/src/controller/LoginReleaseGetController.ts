8e6ea780ce160cd4a8b58a9f03057e59d0b4f22a
gb <13611993+netalkGB@users.noreply.github.com>
2022-11-24 02:23:18 +0900

    クエリのtoに指定された先にリダイレクトする

--- webpilot/src/controller/LoginReleaseGetController.ts
+++ webpilot/src/controller/LoginReleaseGetController.ts
@@ -1,11 +1,16 @@
 import { Request, Response } from 'express'
 import log4js from 'log4js'
+import { AppUtils } from '../utils/AppUtils'
 
 const logger = log4js.getLogger('app')
 
 export const loginReleaseGetController = async (request: Request, response: Response) => {
   logger.info('start superController')
   request.session.destroy((_err) => {
+    if (request.query.to) {
+      response.redirect(AppUtils.urlDecode(request.query.to as string))
+      return
+    }
     response.redirect('/')
   })
   logger.info('end superController')
